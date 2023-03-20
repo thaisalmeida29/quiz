d2b6f566670eda9cea2bca70e390e00937ef94a5
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2023-03-20 19:17:59 -0300

    fazendo umas alterações no context

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -9,14 +9,13 @@ import "./Welcome.css";
 
 const Welcome = () => {
 
-  const quizState = useContext(QuizContext);
-  console.log(quizState);
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div id="welcome">
         <h2>Seja bem-vindo</h2>
         <p>Clique no botão abaixo para começar:</p>
-        <button>Iniciar</button>
+        <button onclick={() => dispatch({ type: "CHANGE_STATE" })}>Iniciar</button>
         <img src={Quiz} alt="Início do Quiz" />
     </div>
   )
